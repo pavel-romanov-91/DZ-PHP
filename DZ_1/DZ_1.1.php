f2c172b4eaee967ac1b429e52e0eeb5471773fd9
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2023-07-13 11:31:24 +0300

    Dz

--- DZ_1/DZ_1.1.php
+++ DZ_1/DZ_1.1.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -60,4 +61,68 @@ printCurrentMonth();
 ?>
 
 </body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<?php
+	function printCurrentMonth() {
+   $dayofmonth = date('t');
+   $day_count = 1;
+   $num = 0;
+   for($i = 0; $i < 7; $i++) 
+	{
+      $dayofweek = date('w', mktime(0, 0, 0, date('m'), $day_count, date('Y')));
+      $dayofweek = $dayofweek - 1;
+      if($dayofweek == -1) $dayofweek = 6;
+      if($dayofweek == $i) {$week[$num][$i] = $day_count;$day_count++;} 
+		else {$week[$num][$i] = "";}
+   }
+   while(true) 
+	{
+      $num++;
+      for($i = 0; $i < 7; $i++) 
+		{
+			$week[$num][$i] = $day_count; $day_count++;
+         if($day_count > $dayofmonth) break;
+      }
+      if($day_count > $dayofmonth) break;
+   }
+   echo date("F Y");
+   echo "<table border=1>";
+   echo "<tr>";
+   echo "<td>Пн</td>";
+   echo "<td>Вт</td>";
+   echo "<td>Ср</td>";
+   echo "<td>Чт</td>";
+   echo "<td>Пт</td>";
+   echo "<td>Сб</td>";
+   echo "<td>Вс</td>";
+   echo "</tr>";
+   for($i = 0; $i < count($week); $i++)
+	{
+   	echo "<tr>";
+      for($j = 0; $j < 7; $j++) 
+		{
+         if(!empty($week[$i][$j])) 
+			{
+        		if($j == 5 || $j == 6) 
+         	echo "<td><font color=red>".$week[$i][$j]."</font></td>";
+         	else echo "<td>".$week[$i][$j]."</td>";
+      	}	else echo "<td>&nbsp;</td>";
+      }
+      echo "</tr>";
+   } 
+   echo "</table>";
+}
+printCurrentMonth();
+?>
+
+</body>
+>>>>>>> c8a75be (Dz)
 </html>
--- DZ_1/DZ_1.php
+++ DZ_1/DZ_1.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -43,3 +44,50 @@ else
 </form>
 </body>
 </html>
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	
+</head>
+<body>
+<?php
+if(isset($_POST['r']) && isset($_POST['g']) && isset($_POST['b']))
+{
+   $color_b = "background:rgb(".$_POST['r'].",".$_POST['g'].",".$_POST['b']." )";
+   $color_s =  "color:rgb(".$_POST['g'].",".$_POST['r'].",".$_POST['b']." )";
+}
+else
+{
+   $color_b = "background:rgb(255,255,255)";
+   $color_s =  "color:rgb(255,51,51)";
+}
+
+?>
+<style>
+	body
+	{
+		<?=$color_b?>;
+	}
+	.span-color
+	{
+		<?=$color_s?>;
+	}
+</style>
+<span class='span-color'>Красивый текст</span>
+<form action='' method='post'>
+	<p>Введите значения цветов</p>
+	<label>R</label>
+	<input type='text' name='r'>
+	<label>G</label>
+	<input type='text' name='g'>
+	<label>B</label>
+	<input type='text' name='b'>
+	<input type='submit' name='sub' value='Accept'>
+</form>
+</body>
+</html>
+>>>>>>> c8a75be (Dz)
