a3cd531435164821a1e7689c72a723fd7e959a8a
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2023-08-21 20:49:42 +0000

    3к

---
+++ DZ_3/tasc_1/lists.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$conn = new mysqli("localhost", "root", "", "dz");
+if($conn->connect_error){
+    die("Ошибка: " . $conn->connect_error);
+}
+$sql = "SELECT * FROM cities";
+if($result = $conn->query($sql)){
+    $rowsCount = $result->num_rows; // количество полученных строк
+    echo "<p>Получено объектов: $rowsCount</p>";
+    echo "<table><tr><th>Id</th><th>Город</th><th>Страна</th></tr>";
+    foreach($result as $row){
+        echo "<tr>";
+            echo "<td>" . $row["id"] . "</td>";
+            echo "<td>" . $row["city"] . "</td>";
+            echo "<td>" . $row["countryid"] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    $result->free();
+} else{
+    echo "Ошибка: " . $conn->error;
+}
+$conn->close();
+?>
+</body>
+</html>
+
+ 
